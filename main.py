5609bfb18ae1352fc0fa7ab5fba0deb84263ad37
Jim Holmström <jim.holmstroem@gmail.com>
2014-10-13 21:51:49 +0200

    moar stuff

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from __future__ import print_function, division
 
 from functools import partial
+from threading import RLock
 import os
 
 from PIL import Image
@@ -12,8 +13,6 @@ import pylab as pl
 import matplotlib.pyplot as plt
 import matplotlib.image as mpimg
 
-pl.ion()
-pl.plot(range(10), range(10))
 
 def composition(f, *g):
     if g:
@@ -25,16 +24,18 @@ def composition(f, *g):
         return f
 
 
-def main(
-    directory_pages='output'
-):
-    page_names = map(
-        partial(os.path.join, directory_pages),
-        os.listdir(directory_pages)
-    )
-    img = mpimg.imread(page_names[0])
-    plt.imshow(img)
-    plt.show()
+i = 0  # NOTE do not judge me
+
+
+def teeprint(template="{}"):
+    def _teeprint(message):
+        print(template.format(message))
+        return message
+    return _teeprint
+
+def mark_rect(filename_template, original_filename, i):
+    filename = 'test.png'
+    print("Starting on part: {}".format(filename))
     upperleft,lowerright = map(
         partial(map, int),
         pl.ginput(2)
@@ -49,14 +50,75 @@ def main(
             upperleft,
         ])
     )
-    img_part = img[
+    img_part_raw = img[
         upperleft[1]:lowerright[1],
         upperleft[0]:lowerright[0],
     ]
 
-    Image.fromarray(
+    img_part = Image.fromarray(
         np.uint8(255*img_part)
-    ).save('test.png')
+    )
+    img_part.save(filename)
+    print('Saved Image part: {} @{} from {}'.format(
+        filename,
+    ))
+    return (upperleft, lowerright), img_part
+
+def render_image(img):
+    print("rerendering..")
+    plt.clf()
+    plt.imshow(img)
+    plt.draw()
+    print("OK")
+
+
+
+def main(
+    directory_pages='output'
+):
+    global i
+    i = 0
+
+    page_names = map(
+        partial(os.path.join, directory_pages),
+        sorted(os.listdir(directory_pages))
+    )
+    imgs = map(
+        composition(mpimg.imread, teeprint('Loading Image: {}')),
+        page_names[:3]
+    )
+    N = len(imgs)
+
+    img = imgs[0]
+    fig = plt.figure(1)
+    plt.show()
+
+    lock = RLock()
+    def onkey(event):
+        try:
+            with lock:
+                if event.key == 'x':
+                    mark_rect('nothing', 'else', 13)
+                else:
+                    global i
+                    if event.key == 'pageup':
+                        i += 1
+                    if event.key == 'pagedown':
+                        i -= 1
+                    i %= N
+                    if i == -1: i = N - 1
+                    print(i)
+                    if event.key == 'pageup' or event.key == 'pagedown':
+                        render_image(imgs[i])
+        except Exception as e:
+            print('Error', e.message)
+
+    fig.canvas.mpl_connect(
+        'key_press_event',
+        onkey
+    )
+
+    render_image(img[i])
 
     pl.close()
 
